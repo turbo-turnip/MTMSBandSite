36bd8182117a552cfb317ae8f8254e83187d2e85
SoftwareFuze <atticus@softwarefuze.com>
2021-04-28 14:09:08 -0700

    Setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import '../main.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(props) {
+    const { entry } = props;
+    return (
+        <React.Fragment>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={entry.home} component={Home} />
+                </Switch>
+            </BrowserRouter>
+        </React.Fragment>
+    );
 }
-
-export default App;
