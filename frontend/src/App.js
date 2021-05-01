4df31f5d5617fdae9c70acc4d28eb1f17ed8d8ac
SoftwareFuze <atticus@softwarefuze.com>
2021-05-01 08:04:06 -0700

    Finished woodwind ages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Register from './Pages/Register/Register';
 import Login from './Pages/Login/Login';
 import Instruments from './Pages/Instruments/Instruments';
 import Brass from './Pages/Instruments/Brass/Brass';
+import Woodwind from './Pages/Instruments/Woodwind/Woodwind';
 
 export default function App(props) {
     const { entry } = props;
@@ -19,6 +20,7 @@ export default function App(props) {
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/instruments" component={Instruments} />
                     <Route exact path="/brass" component={Brass} />
+                    <Route exact path="/woodwind" component={Woodwind} />
                 </Switch>
             </BrowserRouter>
         </React.Fragment>
--- frontend/src/Pages/Instruments/Instrument.jsx
+++ frontend/src/Pages/Instruments/Instrument.jsx
@@ -4,39 +4,42 @@ export default function Instrument(props) {
     return (
         <section className="instrument-content">
             {
-                props.data.type === "Brass" &&
-                <React.Fragment>
-                    <h1>Fingering Charts</h1>
-                    <div className="charts">
-                        {props.data.charts.map(chart => 
-                            <div className="chart">
-                                <h4>{chart.name}</h4>
-                                <a href={chart.location} download>{chart.name.toLowerCase()}</a>
-                                <div class="img">
-                                    <img src="https://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/arrow-download-icon.png" alt="download" />
-                                </div>
-                            </div>)}
-                    </div>
-                </React.Fragment>
+                (props.data.type === "Brass" || props.data.type === "Woodwind") &&
+                    <React.Fragment>
+                        <h1>Fingering Charts</h1>
+                        <div className="charts">
+                            {props.data.charts.map(chart => 
+                                <div className="chart" key={chart.name}>
+                                    <h4>{chart.name}</h4>
+                                    <a href={chart.location} download>{chart.name.toLowerCase()}</a>
+                                    <div className="img">
+                                        <img src="https://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/arrow-download-icon.png" alt="download" />
+                                    </div>
+                                </div>)}
+                        </div>
+                    </React.Fragment>
             }
             <h1>Private Teachers</h1>
             {
                 props.data['private_teachers'].map(teacher => 
-                    <div className="teacher">
+                    <div className="teacher" key={teacher.name}>
                         {teacher.name && <p>{teacher.name} ({teacher.instruments.join(', ')})</p>}
                         {teacher['phone_number'] && <p>{teacher['phone_number']}</p>}
                         {teacher.email && <a href={`mailto:${teacher.email}`}>{teacher.email}</a>}
+                        {teacher.website && <a href={teacher.website} target="_blank" rel="noreferrer">{teacher.website}</a>}
                     </div>)
             }
-            {/* <div className="teacher">
-                <p>Erick E. Escobar (Trumpet)</p>
-                <p>(970) 818-1906</p>
-                <a href="mailto:erick.e.escobar@hotmail.com">erick.e.escobar@hotmail.com</a>
-            </div>
-            <div className="teacher">
-                <p>Ben Medler (Trumpet, Trombone, Bass)</p>
-                <a href="mailto:ben@medlerstudios.com">ben@medlerstudios.com</a>
-            </div> */}
+            {
+                props.data.reeds && 
+                    <React.Fragment>
+                        <h1 className="reeds">Reeds</h1>
+                        {props.data.reeds.map(reed => 
+                            <div className="reed" key={reed.link}>
+                                <h4>{reed.for} — Strength {reed.strength}</h4>
+                                <a href={reed.link} target="_blank" rel="noreferrer">Purchase (amazon.com)</a>
+                            </div>)}
+                    </React.Fragment>
+            }
         </section>
     );
 }
