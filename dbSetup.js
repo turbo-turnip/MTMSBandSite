7167b331bacf1dde18d0867f7eceee6379f4f5f8
SoftwareFuze <atticus@softwarefuze.com>
2021-06-03 18:35:23 -0700

    Prepared for production

--- dbSetup.js
+++ dbSetup.js
@@ -1,11 +1,17 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-	user: process.env.PSQL_USER,
-	password: process.env.PSQL_PASS,
-	host: "localhost",
-	port: 5432,
-	database: "mtmsbandsite"
-});
+const devConfig = {
+    user: process.env.PSQL_USER,
+    password: process.env.PSQL_PASS,
+    host: "localhost",
+    port: 5432,
+    database: "mtmsbandsite"
+};
+
+const proConfig = {
+	connectionString: process.env.DATABASE_URL
+};
+
+const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
 
 module.exports = pool;
