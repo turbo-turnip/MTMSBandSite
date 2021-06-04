d89967a777110830630c6d07586fd75a66ade44d
SoftwareFuze <atticus@softwarefuze.com>
2021-06-04 07:57:47 -0700

    Solving cors errors[9]

--- main.js
+++ main.js
@@ -301,7 +301,7 @@ app.get('/getStats', async (req, res) => {
         res.json({ status: 500 });
     }); */
 	const object = await pool.query("SELECT * FROM stats");
-	object.rows[0].operating_systems = JSON.parse(object.rows[0].operating_systems;
+	object.rows[0].operating_systems = JSON.parse(object.rows[0].operating_systems);
 	object.rows[0].web_browsers = JSON.parse(object.rows[0].web_browsers);
 	object.rows[0].popular_operating_system = JSON.parse(object.rows[0].popular_operating_system);
 	object.rows[0].popular_browser = JSON.parse(object.rows[0].popular_browser);
