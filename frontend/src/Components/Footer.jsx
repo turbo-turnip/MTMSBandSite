c0bcba2b24cd6cad4fcb86e61727753956ba5d5a
Atticus <75766911+SoftwareFuze@users.noreply.github.com>
2022-01-27 14:23:47 -0800

    Update footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -9,11 +9,11 @@ export default function Footer(props) {
                 <div className="section">
                     <h4>Credits</h4>
                     <p>Site created by Atticus Curtis</p>
-                    <p>My Portfolio: <a href="https://softwarefuze.com">softwarefuze.com</a></p>
-                    <p>Discord Account: <span>@TeaKe_smAL#4826</span></p>
-                    <p>Github Account: <a href="https://github.com/SoftwareFuze/">@SoftwareFuze</a></p>
+                    <p>My Portfolio: <a href="https://softwarefuze.ml">softwarefuze.com</a></p>
+                    <p>Discord Account: <span>✨ TeaKe_smAL ✨#4826</span></p>
+                    <p>Github Account: <a href="https://github.com/SoftwareFuze/">SoftwareFuze</a></p>
                     <p>Codepen Account: <a href="https://codepen.io/teake_smal/">TeaKe_smAL</a></p>
-                    <p>Contact me at: <a href="mailto:atticus@softwarefuze.com">atticus@softwarefuze.com</a></p>
+                    <p>Contact me at: <a href="mailto:att.cubing.sharks@gmail.com">att.cubing.sharks@gmail.com</a></p>
                 </div>
                 <div className="section">
                     <h4>Site Resources</h4>
@@ -21,7 +21,7 @@ export default function Footer(props) {
                     <p>Github Repo: <a href="https://github.com/SoftwareFuze/MTMSBandSite-Frontend">Frontend</a></p>
                     <p>Changelog: <Link to="/changelog">mtmsband.netlify.app/changelog</Link></p>
                     <p>Site Created with 💛 and the ERN stack. (Express, ReactJS, and NodeJS)</p>
-                    <p>Need me to build your site? Email me at <a href="mailto:atticus@softwarefuze.com">atticus@softwarefuze.com</a>!</p>
+                    <p>Need me to build your site? Email me at <a href="mailto:att.cubing.sharks@gmail.com">att.cubing.sharks@gmail.com</a>!</p>
                 </div>
             </div>
         </footer>
